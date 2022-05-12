3242576835c9f84ec398fe31db7892c3a5a3c1ac
Ralph-Chae <103868581+Ralph-Chae@users.noreply.github.com>
2022-05-12 21:22:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const Player = {
-    Name: "chae",
-    Points: 10,
-    fat: true,
-    };
-
-    console.log (player)
-    console.log(player.Name)
-    player.Name
+if(8 >10){
+    console.log("hi")
+}else if(8 < 10){
+    console.log("hello")
+}else{
+    console.log("bye")
+}
